3656c038756dbcb95ec53065e392fc303946f693
Michael <kasmickleva@gmail.com>
2018-12-31 15:15:37 +0100

    fix(server): fix functions to create, join, leave and delete chatroom

--- server/client.js
+++ server/client.js
@@ -6,28 +6,6 @@ class Client {
     this.socket = socket;
     this.chatroom = null;
   }
-
-  /**
-   * @return {string} the client's current chatroom
-   */
-  get chatroom() {
-    return this.chatroom;
-  }
-
-  /**
-   * @param {string} chatroom the name of the client chatroom
-   */
-  set name(chatroom) {
-    this.chatroom = chatroom;
-  }
-
-  get color() {
-    return this.color;
-  }
-
-  set color(color) {
-    this.color = color;
-  }
 }
 
 export default Client;
